6d7005059c2692dd78fe289f823b5d9c012d70ae
riarrodr <122411873+riarrodr@users.noreply.github.com>
2023-04-18 00:40:19 -0700

    removed debug code

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -11,15 +11,4 @@ def hello_world():
     return 'Hello Flafdsdk!!d'
 
 if __name__ == '__main__':
-    print(client.list_database_names())
-    print("\n\n\n")
-    db = client["testdb"]
-    coll = db["testcoll"]
-    post1 = {"name": "hello"}
-    coll.insert_one(post1)
-    results = coll.find({})
-    print("found results:\n")
-    for r in results:
-        print(r)
-    print("\n end results")
     app.run(debug = True, host = '0.0.0.0', port = 5001)
