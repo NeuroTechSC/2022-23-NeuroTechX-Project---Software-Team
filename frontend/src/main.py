1ced06ac1708259fc1fc64c8e6ca8a03d4757ce7
Anthony Carmona <ac5891358@gmail.com>
2023-04-26 11:28:25 -0700

    Second attempt at exporting

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -2,13 +2,37 @@ from flask import Flask
 import requests
 import pymongo
 from pymongo import MongoClient
+from io import BytesIO
+from flask import Flask, send_file
+#import requests
+#import pymongo
+#from pymongo import MongoClient
+import csv
 
 app = Flask(__name__)
 client = MongoClient('mongodb://root:example@mongo:27017')
+db = client['mydatabase']
+collection = db['mycollection']
 
 @app.route('/')
 def hello_world():
     return 'Hello Flafdsdk!!d'
+
+@app.route('/export')
+def export_data():
+    # Grabbing the data from MongoDB
+    data = collection.find()
+     #Writing to a CSV File
+    display = BytesIO()
+    writer  = csv.writer(display)
+    for text in data:
+        writer.writerow([text['_id'], text['name']])
+    with open("data.csv", "wb") as f:
+        f.write(display.getbuffer())
+    #Close CSV file to user
+    display.seek(0)
+    return send_file('data.csv', as_attachment=True)
+
 
 if __name__ == '__main__':
     print(client.list_database_names())
